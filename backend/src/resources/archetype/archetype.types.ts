d422c71dbfe2ee7829c1ee06e4636d1f263d1e65
Vinicius Fonseca <vinicius.fonseca@icomp.ufam.edu.br>
2025-09-18 17:12:44 -0400

    ajustando archetype.types para o padrão do projeto

--- backend/src/resources/archetype/archetype.types.ts
+++ backend/src/resources/archetype/archetype.types.ts
@@ -1,6 +1,8 @@
 import { Archetype } from '@prisma/client';
 
-export type CreateArchetypeDTO = Pick<Archetype, "name" | "hp" | "mp" | "tp">;
-export type UpdateArchetypeDTO = Partial<CreateArchetypeDTO>;
-export type DeleteArchetypeDTO = Pick<Archetype, 'id'>;
-export type ArchetypeDTO = Archetype;
+type CreateArchetypeDTO = Pick<Archetype, "name" | "hp" | "mp" | "tp">;
+type UpdateArchetypeDTO = Partial<CreateArchetypeDTO>;
+type DeleteArchetypeDTO = Pick<Archetype, 'id'>;
+type ArchetypeDTO = Archetype;
+
+export { CreateArchetypeDTO, UpdateArchetypeDTO, DeleteArchetypeDTO, ArchetypeDTO };
