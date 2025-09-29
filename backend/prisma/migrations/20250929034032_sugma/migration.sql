f814523a9516932bd03af5bc59a73d2198884176
Vinicius Fonseca <vinicius.fonseca@icomp.ufam.edu.br>
2025-09-28 23:41:28 -0400

    tirando um negocio q num era pra te no crisma

---
+++ backend/prisma/migrations/20250929034032_sugma/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `formula` on the `item_skills` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `item_skills` DROP COLUMN `formula`;
